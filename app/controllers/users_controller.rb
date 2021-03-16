939ff4d67ce68302e7068bd4508c76d7bc17a338
somoye123 <somoyeayotunde@gmail.com>
2021-03-16 10:29:09 +0100

    utilize ordered_colum scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.all.ordered_column
   end
 
   # GET /users/1 or /users/1.json
