c8aa5bca962f7bf087108629ba31b94dbf1cb9c7
somoye123 <somoyeayotunde@gmail.com>
2021-03-16 10:36:21 +0100

    validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, :email, :title, :status, :phone, presence: true
-
+  validates :email, uniqueness: { case_sensitive: false }
   scope :ordered_column, lambda {
                            order(updated_at: :desc).order(name: :asc).order(email: :asc).order(title: :asc).order(phone: :asc).order(status: :asc)
                          }
