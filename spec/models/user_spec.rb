6fea54dea56fdb4213e57fc41a2d68ea853ddfca
somoye123 <somoyeayotunde@gmail.com>
2021-03-17 05:39:34 +0100

    write unit test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it {
+      should allow_values('somoye@gmail.com', 'ayoutunde@yahoo.com')
+        .for(:email)
+    }
+
+    it {
+      should_not allow_values('somoye', 'ayotunde')
+        .for(:email)
+    }
+  end
 end
