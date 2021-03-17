59559fc3a8595a69062b92def14e46d02a1f1596
somoye123 <somoyeayotunde@gmail.com>
2021-03-17 05:39:21 +0100

    refactor user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
   validates :name, :email, :title, :status, :phone, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :status, inclusion: { in: %w[active inactive],
+                                  message: '%{value} is not a valid status' }
+
   scope :ordered_column, lambda {
                            order(updated_at: :desc).order(name: :asc).order(email: :asc).order(title: :asc).order(phone: :asc).order(status: :asc)
                          }
